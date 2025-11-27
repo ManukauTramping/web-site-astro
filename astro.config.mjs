b2476e9f3987e8b11ee4756cf5eed4e5c3fcec19
Colin Henwood <aretenz@users.noreply.github.com>
2025-11-27 20:31:23 +1300

    Remove packages not required

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 //import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
   image: {
     domains: ['images.ctfassets.net'],
     remotePatterns: [{ protocol: "https" }],
