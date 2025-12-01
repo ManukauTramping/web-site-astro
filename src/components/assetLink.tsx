4e5ede08673de259786b169d76f58567a0c7ad95
manukautrampingclub <61818232+manukautrampingclub@users.noreply.github.com>
2025-12-01 22:09:35 +1300

    Fix rich-text asset-link rendering (#8)
    Co-authored-by: Colin Henwood <aretenz@users.noreply.github.com>

---
+++ src/components/assetLink.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import type { Block, Inline, Link, Text } from "@contentful/rich-text-types";
+import { contentfulClient } from "../lib/contentful.ts";
+
+interface Props {
+  target: Link<'Asset'>,
+  content: Block | Inline | Text,
+}
+
+const AssetLink = async ({ target, content } : Props) => {
+  const asset = await contentfulClient.getAsset(target.sys.id);
+
+  if (!asset || !asset.fields || !asset.fields.file) {
+    return <></>;
+  }
+
+  const file= asset.fields.file;
+  return (
+    file.contentType.startsWith('image')
+      ? <img
+          src={file.url}
+          alt={content.toString()}
+          decoding={'async'}
+          loading={'lazy'}
+          height={file.details.image?.height}
+          width={file.details.image?.width}
+        />
+      : <a
+          href={file.url}
+          type={file.contentType}
+          download
+          className={'link-color'}
+          target={'_blank'}
+        >
+          {file.fileName}
+        </a>
+  )
+}
+
+export default AssetLink;
